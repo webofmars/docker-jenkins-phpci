e322f77efb21b081d7f6f8b202f1396994954647
Frederic Leger <frederic@webofmars.com>
2016-05-26 01:44:19 +0200

    better docker gid workaround

--- set-docker-gid.sh
+++ set-docker-gid.sh
@@ -3,6 +3,7 @@
 DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
 
 # FIXME : $DOCKER_GID must exists for now
+groupadd fakedocker --gid $DOCKER_GID
 usermod -a -G $DOCKER_GID  jenkins
-echo "added jenkins to $DOCKER_GID group (used by docker on underlying system)"
-
+echo "added jenkins to $DOCKER_GID group (used by docker on underlying system): "
+grep $DOCKER_GID /etc/group
