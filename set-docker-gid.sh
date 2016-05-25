2c8399ae2e498d394367a2dd438b477705872bc8
Frederic Leger <frederic@webofmars.com>
2016-05-26 01:09:42 +0200

    clean and fix in docker socket workaround

--- set-docker-gid.sh
+++ set-docker-gid.sh
@@ -1,6 +1,8 @@
-﻿#!/bin/bash
+#!/bin/bash
 
 DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
+
+# FIXME : $DOCKER_GID must exists for now
 usermod -a -G $DOCKER_GID  jenkins
 echo "added jenkins to $DOCKER_GID group (used by docker on underlying system)"
 
